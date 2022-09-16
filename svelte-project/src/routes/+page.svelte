ccaaf3fb37d0406370916b8b959f0bfcdbaab8d2
Allen May <allenhmay@gmail.com>
2022-09-16 11:54:50 -0400

    Final commit

--- svelte-project/src/routes/+page.svelte
+++ svelte-project/src/routes/+page.svelte
@@ -1,32 +1,54 @@
-
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
 <script lang="ts">
     async function getItems() {
         const res = await fetch('https://testapi.io/api/ndenlinger/roveiq');
         const items = await res.json();
-
-        // console.log('the items', items.data.css);
 
         if (res.ok) {
             return items;
         } else {
             throw new Error(items);
         }
-    }
-    
+    }    
     $: allItemsPromise = getItems();
 </script>
+<section class="globalHome">
+    <div class="b1">List of Locations:
+        {#await allItemsPromise then items}
+        {#each items.data.locations as location}
+        <article>
+            <a href="{location.id}">{location.name}
+        </article>
+        {/each}            
+        {/await}
+    </div>
+    <div class="b2">
+        {#await allItemsPromise then items}
+           <img src="{items.data.locations[1].banner_img}" alt=""> 
+        {/await}
+    </div>
+    <div class="b3">Ad Slideshow1</div>
+    <div class="b4">
+        {#await allItemsPromise then items}
+        <img src="{items.data.locations[1].logo_img}" alt="">
+    {/await}
+    </div>
+    <div class="b5">
+        {#await allItemsPromise then items}
+            {items.data.locations[1].name}
+        {/await}
 
-<div>
-    {#await allItemsPromise then items}    
-    <div>{items.data.kiosk.name}</div>
-    <div>{items.data.screen.name}</div>
-    {/await}    
-</div>
-<svelte:head>
-    <style>
-            {allItemsPromise.data.css};
-    </style>
-</svelte:head>
+    </div>
+    <div class="b6">
+        {#await allItemsPromise then items}
+        <p>
+            {items.data.locations[1].address1}
+            {items.data.locations[1].city},
+            {items.data.locations[1].state}
+            {items.data.locations[1].zip}
+        </p>
+        <p>
+            {items.data.locations[1].description}
+        </p>
+        {/await}
+    </div>
+</section>
